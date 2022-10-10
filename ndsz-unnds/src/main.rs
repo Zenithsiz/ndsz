d92811bc204bd809b4c0cdd5f76457a2033b2b8d
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-10-10 15:01:48 +0100

    Extracted parsing functions in `unnds`.

--- ndsz-unnds/src/main.rs
+++ ndsz-unnds/src/main.rs
@@ -21,7 +21,7 @@ use {
 	clap::Parser,
 	ndsz_fat::{FileAllocationTable, FileNameTable},
 	ndsz_util::{IoSlice, ReadByteArray},
-	std::fs,
+	std::{fs, io},
 	tracing_subscriber::prelude::*,
 };
 
@@ -45,65 +45,76 @@ fn main() -> Result<(), anyhow::Error> {
 	let mut input_file = fs::File::open(&args.input_path).context("Unable to open input file")?;
 
 	// Read the header
-	let header = {
-		let header_bytes = input_file.read_byte_array().context("Unable to read header")?;
-		ndsz_nds::Header::from_bytes(&header_bytes).context("Unable to parse header")?
-	};
+	let header = parse_header(&mut input_file)?;
 	tracing::trace!(?header);
 
-	// Get the fat
-	let fat = {
-		// Get the fat
-		let mut fat_slice = IoSlice::new_with_offset_len(
-			&mut input_file,
-			u64::from(header.file_allocation_table.offset),
-			u64::from(header.file_allocation_table.length),
-		)
-		.context("Unable to create fat slice")?;
+	// Get the fat, then output it, and it's files, if requested
+	let fat = self::parse_fat(&mut input_file, &header)?;
 
-		FileAllocationTable::from_reader(&mut fat_slice).context("Unable to get fat")?
-	};
-
-	// Output the fat, if requested
 	if args.fat {
 		let fat_path = output_path.join("fat.yaml");
 		self::output_fat_yaml(&fat, fat_path).context("Unable to output fat yaml")?;
 	}
 
-	// Output the fat files, if requested
 	if args.fat_files {
 		self::extract_fat_raw(&fat, &mut input_file, &output_path)?;
 	}
 
-	// Get the fnt
-	let fnt = {
-		// Get the fnt
-		let mut fnt_slice = IoSlice::new_with_offset_len(
-			&mut input_file,
-			u64::from(header.file_name_table.offset),
-			u64::from(header.file_name_table.length),
-		)
-		.context("Unable to create fnt slice")?;
+	// Get the fnt, then output it, if requested
+	let fnt = self::parse_fnt(&mut input_file, &header)?;
 
-		// And read it
-		FileNameTable::from_reader(&mut fnt_slice).context("Unable to read fnt")?
-	};
-
-	// Output the fnt, if requested
 	if args.fnt {
 		let fnt_path = output_path.join("fnt.yaml");
 		self::output_fnt_yaml(&fnt, fnt_path).context("Unable to output fnt yaml")?;
 	}
 
-	// Create the output directory if it doesn't exist
+	// Then extract all parts, and the directory structure
 	fs::create_dir_all(&output_path).context("Unable to create output directory")?;
 
-	// Extract all nds files
 	self::extract_all_parts(&mut input_file, &header, &output_path).context("Unable to extract parts")?;
 
-	// Extract the filesystem
 	let fs_dir = output_path.join("fs");
 	self::extract_fat_dir(&fnt.root, &mut input_file, &fat, fs_dir).context("Unable to extract fat")?;
 
 	Ok(())
+}
+
+/// Parses the header
+fn parse_header<R: io::Read>(rom_file: &mut R) -> Result<ndsz_nds::Header, anyhow::Error> {
+	let header_bytes = rom_file.read_byte_array().context("Unable to read header")?;
+	ndsz_nds::Header::from_bytes(&header_bytes).context("Unable to parse header")
+}
+
+/// Parses the fat
+fn parse_fat<R: io::Read + io::Seek>(
+	rom_file: &mut R,
+	header: &ndsz_nds::Header,
+) -> Result<FileAllocationTable, anyhow::Error> {
+	// Create the slice
+	let mut fat_slice = IoSlice::new_with_offset_len(
+		rom_file,
+		u64::from(header.file_allocation_table.offset),
+		u64::from(header.file_allocation_table.length),
+	)
+	.context("Unable to create fat slice")?;
+
+	// Then parse it
+	FileAllocationTable::from_reader(&mut fat_slice).context("Unable to get fat")
+}
+
+/// Parses the fnt
+fn parse_fnt<R: io::Read + io::Seek>(
+	rom_file: &mut R,
+	header: &ndsz_nds::Header,
+) -> Result<FileNameTable, anyhow::Error> {
+	// Create the slice
+	let mut fnt_slice = IoSlice::new_with_offset_len(
+		rom_file,
+		u64::from(header.file_name_table.offset),
+		u64::from(header.file_name_table.length),
+	)
+	.context("Unable to create fnt slice")?;
+
+	// Then parse it
+	FileNameTable::from_reader(&mut fnt_slice).context("Unable to read fnt")
 }
